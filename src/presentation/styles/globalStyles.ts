21657fe64cb66d7b6ead9729393159c8fc642dfa
João Gabriel Campos <joaaogabriel.oi@hotmail.com>
2021-12-16 15:22:25 -0300

    feat: create global styles

---
+++ src/presentation/styles/globalStyles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+:root {
+  --pruple-pastel: #B983FF;
+  --white: #ffffff;
+  --dark-color:#4D2B79;
+  --blue-pastel:#94B3FD;
+}
+  *{
+    margin:0;
+    padding:0;
+    border:0;
+  }
+  root{}
+  body{
+    background:#f0f0f5;
+    font-family: 'IBM Plex Sans', sans-serif;
+      
+  }
+  h1,h2,h3,h4,h5{
+    font-family: 'Comfortaa', cursive;
+  }
+  
+  h1{
+    font-size:46px;
+  }
+  h2{
+    font-size:36px;
+  }
+  h3{
+    font-size:32px;
+  }
+  h4{
+    font-size:28px;
+  }
+  h5{
+    font-size:24px;
+  }
+  
+  `
